87bb61e1a0c645ed8f1df9b43ef1ff6e6fd7e0ac
flavio.paiva <na519631na*>
2020-09-09 14:42:55 -0400

    Step 7: Add ESLint Code Linting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import './css/style.css'
 // Log message to console
 logMessage('A very warm welcome to Expack!')
 // Needed for Hot Module Replacement
+
+// eslint-disable-next-line no-undef
 if(typeof(module.hot) !== 'undefined') {
   module.hot.accept() // eslint-disable-line no-undef  
 }
+
+//module.hot.accept() // eslint-disable-line no-undef
--- src/js/logger.js
+++ src/js/logger.js
@@ -1,2 +1,3 @@
+// eslint-disable-next-line no-undef
 const logMessage = msg => console.log(msg)
 export default logMessage
